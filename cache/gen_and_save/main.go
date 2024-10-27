324d29ef3b950c2b18662c7ad9d40e62d86cfe20
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-10-27 15:52:57 +0900

    feat: cache normal

---
+++ cache/gen_and_save/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	mCache := NewCache()
+
+	for i := 0; i < 20; i++ {
+		key := rand.Intn(5) + 1
+		value := mCache.Get(key)
+		log.Println(value)
+	}
+}
+
+type Cache struct {
+	mu    sync.Mutex
+	items map[int]int
+}
+
+func NewCache() *Cache {
+	m := make(map[int]int)
+	c := &Cache{items: m}
+
+	return c
+}
+
+func (c *Cache) Set(key int, value int) {
+	c.mu.Lock()
+	c.items[key] = value
+	c.mu.Unlock()
+}
+
+func (c *Cache) Get(key int) int {
+	c.mu.Lock()
+	v, ok := c.items[key]
+	c.mu.Unlock()
+
+	if ok {
+		return v
+	}
+
+	// もしなかったらDBにアクセス
+	v = GetFromDB(key)
+	// キャッシュに保存
+	c.Set(key, v)
+
+	return v
+}
+
+func GetFromDB(key int) int {
+	time.Sleep(time.Second)
+	return key * 2
+}
